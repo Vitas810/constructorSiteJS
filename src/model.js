c2fb86c945f9cf6efd1fbcc36413e4f5c76c43ce
Vitas810 <59773453+Vitas810@users.noreply.github.com>
2020-11-03 21:32:31 +0700

    создание модульности

---
+++ src/model.js
@@ -0,0 +1,10 @@
+export const model = [
+  { type: 'title', value: 'hello world' },
+  {
+    type: 'text',
+    value:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed iureea enim fugiat nam magni.',
+  },
+  { type: 'columns', value: ['1111111', '2222222', '3333333', '44444444'] },
+  { type: 'image', value: './assets/image.png' },
+];
