1853f5e5cabaf571b72b779f0c7a31fb204fffb7
Vitas810 <59773453+Vitas810@users.noreply.github.com>
2020-11-03 22:13:15 +0700

    создание доп фунций, унифицирование файла templates

--- src/templates.js
+++ src/templates.js
@@ -1,38 +1,22 @@
-export function title(block) {
-  return `
-        <div class="row">
-          <div class="col-sm">
-            <h1>${block.value}</h1>
-          </div>
-        </div>
-    
-    `;
-}
-export function text(block) {
-  return `
-      <div class="row">
-        <div class="col-sm">
-          <p>
-          ${block.value}
-          </p>
-        </div>
-      </div>
-    
-    `;
-}
-export function columns(block) {
-  const html = block.value.map((item) => ` <div class="col-sm">${item}</div>`);
+import { row, col } from './utils';
 
-  return `
-      <div class="row">
-      ${html.join('')}
-     </div>
-    `;
+function title(block) {
+  return row(col(`<h1>${block.value}</h1>`));
 }
-export function image(block) {
-  return `
-   <div class="row">
-   <img src=" ${block.value}" />
-   </div>
-  `;
+function text(block) {
+  return row(col(`<p>${block.value}</p>`));
 }
+function columns(block) {
+  const html = block.value.map((item) => ` <div class="col-sm">${item}</div>`);
+  return row(html.join(''));
+}
+function image(block) {
+  return row(`<img src=" ${block.value}" />`);
+}
+
+export const templates = {
+  title,
+  text,
+  columns,
+  image,
+};
